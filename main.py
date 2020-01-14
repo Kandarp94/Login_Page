c38b5959508291c3513e56414356506f4eb2e4a3
Kandarp94 <kandarpsharma94skt@gmail.com>
2020-01-15 01:19:43 +0530

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+
 
 app = Flask(__name__)
-
-# Change this to your secret key (can be anything, it's for extra protection)
-# app.secret_key = 'your secret key'
 
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
@@ -22,40 +19,43 @@ mysql = MySQL(app)
 @app.route('/accounts/login/', methods=['GET', 'POST'])
 def login():
     msg = ''
-    email=''
-    password = ''
+    email = ' '
+    password = ' '
     # Check if "email" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        # Create variables for easy access
+       
         email = request.form['email']
         password = request.form['password']
+        print(email,password)
 
-     # Check if account exists using MySQL
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
-    # Fetch one record and return result
-    account = cursor.fetchone()
-    name = account["first_name"]
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        print(account)    
+        # If account exists in accounts table in out database
+        if account:
+            # Redirect to home page
+            name = account['first_name']
+            return render_template('home.html',username = name)
+        else:
+            # Account doesnt exist or username/password incorrect
+            print("in else")
+            msg = 'Incorrect username/password!'
         
-    # If account exists in accounts table in out database
-    if account:
-        # Redirect to home page
-        return render_template('home.html',username = name)
-    else:
-        # Account doesnt exist or username/password incorrect
-        msg = 'Incorrect username/password!'
+        print(msg)
 
-    return render_template('index.html', msg=msg)
+    return render_template('index.html', msg = msg)
 
 
 # http://localhost:5000/accounts/signup - this will be the registration page, we need to use both GET and POST requests
 @app.route('/accounts/signup', methods=['GET', 'POST'])
 def signup():
-    # Output message if something goes wrong...
-    msg = ''
+    
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'first_name' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
+       
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
